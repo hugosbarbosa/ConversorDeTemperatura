5ea1215643b3fdba5e9032873f7b0f6d8d834090
hugobarbosa <125314621+hugosbarbosa@users.noreply.github.com>
2024-10-29 20:42:35 -0300

    add

--- ConversorDeTemperatura/Program.cs
+++ ConversorDeTemperatura/Program.cs
@@ -10,6 +10,22 @@ namespace ConversorDeTemperatura
     {
         static void Main(string[] args)
         {
+             double c, f, k;
+
+
+
+ Console.WriteLine("==== CONVERSOR DE TEMPERATURAS ====");
+ Console.Write("Insira a temperatura em Celsius: ");
+ c = double.Parse(Console.ReadLine());
+ Console.WriteLine("---------------------------------");
+
+ f = (c * 9 / 5) + 32; // (c * 9 / 5) + 32 = F
+ k = c + 273.15; // c + 273,15
+
+ Console.WriteLine(c + " graus celsius = " + f + " graus fahrenheit");
+ Console.WriteLine(c + " graus celsius = " + k + " graus kelvin");
+ Console.WriteLine("---------------------------------");
+ Console.ReadKey();
         }
     }
 }
